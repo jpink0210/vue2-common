a76acc8b257d3e4eb7fa937ebe436807c9529052
焦品貴 <jpink0210@gmail.com>
2023-04-21 14:35:06 +0800

    [Page] 把以前實驗的元件內容做定位和嚴格的使用時機

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    this is demo
-      <chech-box-ary-input
-        v-for="(item, index) in Array.from(Array(10).keys())"
-        :key="`item-check-${index}`"
-        :value="item"
-        v-bind="{
-          localValue
-        }"
-        @change="change"
-        @checkedChange="checkedChange"
-      >
-        {{ item }}
-      </chech-box-ary-input>
 
-      <CheckBox />
+    <p>我確實都是使用了： export default Vue.extend，為了釋出</p>    
+
+    <p>第一個是模仿：vip 嚴格地義版本的</p>
+
+    <chech-box-ary-input
+      v-for="(item, index) in Array.from(Array(10).keys())"
+      :key="`item-check-${index}`"
+      :value="item"
+      v-bind="{
+        localValue
+      }"
+      @change="change"
+      @checkedChange="checkedChange"
+    >
+      {{ item }}
+    </chech-box-ary-input>
+    <p>第二個是模仿 cvue</p>
+    <CheckBox />
   </div>
 </template>
 
