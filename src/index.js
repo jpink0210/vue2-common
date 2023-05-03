4fceb6b88964f2abb8157a6718fe62714a44027b
焦品貴 <jpink0210@gmail.com>
2023-05-03 16:24:43 +0800

    [Select] Export

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import CheckBox from "./components/inputs/CheckBox.vue";
 import FormInput from "./components/inputs/FormInput";
 import FormRadio from "./components/inputs/FormRadio";
 import FormCheckbox from "./components/inputs/FormCheckbox";
+import Select from "./components/forms/Select";
 
 export {
     ChechBoxAryInput,
     CheckBox,
     FormInput,
     FormRadio,
-    FormCheckbox
+    FormCheckbox,
+    Select
 }
