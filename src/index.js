fc56190ad042bda1d845dbcd2b848f2dc6f3f81d
焦品貴 <jpink0210@gmail.com>
2023-05-04 14:30:31 +0800

    [SelectMultiple] Export

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import ChechBoxAryInput from "./components/ChechBoxAryInput.vue";
-import CheckBox from "./components/inputs/CheckBox.vue";
 import FormInput from "./components/inputs/FormInput";
 import FormRadio from "./components/inputs/FormRadio";
 import FormCheckbox from "./components/inputs/FormCheckbox";
 import Select from "./components/forms/Select";
+import SelectMultiple from "./components/forms/SelectMultiple";
 import Tag from "./components/tag/Tag";
 
 export {
-    ChechBoxAryInput,
-    CheckBox,
     FormInput,
     FormRadio,
     FormCheckbox,
     Select,
+    SelectMultiple,
     Tag
 }
